43f4d6880b7e8584b502133fa0177a0ffb90c9cf
Daniel Joo <daniel.h.joo@medtronic.com>
2025-08-27 13:51:58 -0700

    add edit invoice page

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,28 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+    // once you land on this page, get the invoice id from the url params
+    // fetch invoice from db with the invoice id, then return the form with the invoice data
+    // create a server action to update the form data
+    // validate data
+    // handle errors
+    // update db
+    // send user back to invoice page
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    );
+}
